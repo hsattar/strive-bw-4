41174edaab8b3d8e298a36176f152966a37e7499
martipurull <91315241+martipurull@users.noreply.github.com>
2022-02-23 15:42:39 +0100

    sends message

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,11 @@ import { Grid } from "@mui/material"
 import Conversations from "./conversations/Conversations"
 import Sidebar from "./Sidebar"
 
+
 export default function Home() {
+
+
+
   return (
     <Grid container>
       <Grid item xs={3}>
